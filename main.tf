3166e65f723c488495f339c8457b29812be983f0
Dennis Felsing <dennis@felsing.org>
2025-01-21 22:07:59 +0000

    Upgrade terraform-helm-materialize refernece

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "storage" {
 }
 
 module "operator" {
-  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.0"
+  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.1"
 
   count = var.install_materialize_operator ? 1 : 0
 
