1757c6b3c429498bdd3bcf4c8fbfac6b2336a07c
Alex Hunt <alex.hunt@materialize.com>
2025-03-25 13:30:32 +0000

    Bump terraform-helm-materialize to v0.1.9

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "certificates" {
 }
 
 module "operator" {
-  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.8"
+  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.9"
 
   count = var.install_materialize_operator ? 1 : 0
 
